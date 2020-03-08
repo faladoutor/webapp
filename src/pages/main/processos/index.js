7269ff208d834661dc88322a0ff72cb63c2f09f3
Charley Oliveira <charleycesar@gmail.com>
2020-03-08 15:11:51 -0300

    primeiro processo

--- src/pages/main/processos/index.js
+++ src/pages/main/processos/index.js
@@ -1,15 +1,57 @@
 import React, {Component} from 'react';
 import {View, FlatList, Text} from 'react-native';
 
+const processosData = [
+  {
+    numero: '0022148-94.2019.8.19.0208',
+    juizado: '12 JUIZADO ESPECIAL CIVEL - REGIONAL DO MEIER',
+    autor: [
+      {
+        nome: 'CENTRO EDUCACIONAL DA INFANCIA UNIDUNITE',
+        advogado: 'ALZIR PANTALEAO DE MELLO ALVES',
+        oab: '075856',
+      },
+    ],
+    reu: [
+      {
+        nome: 'MARIANA AMORIM SILVA',
+      },
+    ],
+    movimentacoes: [
+      {
+        data: '06/03/2020',
+        status: 'JUNTADA',
+        traducao: 'Documento eletrônico juntado de forma automática.',
+      },
+      {
+        data: '02/03/2020',
+        status: 'ENVIO DE DOCUMENTO ELETRONICO.',
+        traducao: '',
+      },
+      {
+        data: '28/02/2020',
+        status: 'RECEBIMENTO',
+        traducao: '',
+      },
+      {
+        data: '28/02/2020',
+        status: 'DESPACHO',
+        traducao:
+          '1 - Intime-se a parte autora para, no prazo de 15 (quinze) dias, manifestar-se sobre proposta de parcelamento indicada pelo réu às fls. 100/101. 2 - Intime-se a parte ré para, no prazo de 15 (quinze) dias, comprovar nos autos a realização de depósito judicial á disposição deste juízo. Após, certificados, voltem cls.',
+      },
+    ],
+  },
+];
+
 class ProcessosPage extends Component {
   renderItem = data => {
     return (
       <View key={data.id} style={styles.containerItem}>
         <View style={styles.nameRowContainer}>
-          <Text style={styles.lblName}>{data.name}</Text>
+          <Text style={styles.lblName}>{data.reu[0].nome}</Text>
           <Text style={styles.lblNormal}>{data.status}</Text>
         </View>
-        <Text style={styles.lblNormal}>N. Processo: {data.id}</Text>
+        <Text style={styles.lblNormal}>N. Processo: {data.numero}</Text>
       </View>
     );
   };
@@ -18,10 +60,7 @@ class ProcessosPage extends Component {
     return (
       <View style={styles.container}>
         <FlatList
-          data={[
-            {id: 1, name: 'Charley Oliveira', status: 'APROVADO'},
-            {id: 1, name: 'Charley Oliveira', status: 'APROVADO'},
-          ]}
+          data={processosData}
           renderItem={({item}) => this.renderItem(item)}
           keyExtractor={item => item.id}
         />
